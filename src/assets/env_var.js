bbbb0afa9631ee9ec4ce34fe3c6d3a0a880dd4cb
pk856j <pk856j@att.com>
2021-03-08 18:58:51 +0530

    'addingcredentialsId

--- src/assets/env_var.js
+++ src/assets/env_var.js
@@ -1,6 +1,6 @@
 // Add your own google Auth Credentials //
 
   export const keys  = {
-    credential_id: ''
+    credential_id: '289206687847-pfc7k20mscl1qiquf5vk0oveo8ru17oc.apps.googleusercontent.com'
 }
 
